e22aab1de9ce49d1c1bc6be8191a62c899a54fbc
Luke Vaughan <lvaughan@login01.af.uchicago.edu>
2023-11-02 16:06:02 -0500

    balance + test train split

--- prepare_dataset/get_data.cpp
+++ prepare_dataset/get_data.cpp
@@ -31,42 +31,50 @@ int get_nlines(char filename[]){
 vector<string> get_line_data(string line_data){
   vector<string> nums;
   int N = line_data.length();
-  int idx1, idx2 = 0;
+  int idx1 =0;
+  int idx2 = 0;
+  //int n=0;
   //cout << line_data << endl;
 
   for (int i=0; i<N; i++){
     if ((line_data[i]==' ')){
       idx2 = i;
+      //cout << "N " << N << " idx1 " << idx1 << " idx2 " << idx2 << " n " << n << endl;
       nums.push_back(line_data.substr(idx1, idx2-idx1));
       idx1 = idx2+1;
+      //n++;
     }
   }
+  //cout << "N " << N << " idx1 " << idx1 << " idx2 " << idx2 << " n " << n << endl;
   nums.push_back(line_data.substr(idx1,N-idx1));
   return nums;
 }
 
-vector<vector<vector<string> > > get_data(char filename[], int nlines, int maxjets){
-fstream myfile;
+vector<vector<vector<string> > > get_data(char filename[], int nlines, int maxjets, int nvars){
+  fstream myfile;
  
-    // open file
-    myfile.open(filename);
-    vector<string> g1;
- 
-    if (myfile.is_open()) {
-        // checking if the file is open
-        string str;
- 
-        // read data from file object
-        // and put it into string.
-        //while (getline(myfile, str, ' ')) {
-        while (getline(myfile, str)) {
-            g1.push_back(str);
-        }
-        // close the file object.
-        myfile.close();
-   }
+  // open file
+  myfile.open(filename);
+  //vector<string> g1(nlines);
+  vector<string> g1;
 
-  /*
+  if (myfile.is_open()) {
+    // checking if the file is open
+    string str;
+    int i=0;
+ 
+    // read data from file object
+    // and put it into string.
+    //while (getline(myfile, str, ' ')) {
+    while (getline(myfile, str)) {
+      //g1[i++]=str;  
+      g1.push_back(str);
+    }
+    // close the file object.
+    myfile.close();
+ }
+
+  /* 
   cout << "\nVector elements are: " << endl;
   for (int i = 0; i < g1.size(); i++) {
         cout << g1[i] << endl;
@@ -74,15 +82,15 @@ fstream myfile;
   */
 
   int events = int(nlines / maxjets);
-  vector<vector<vector<string> > > data(events, vector<vector<string> >(maxjets, vector<string>(4)));
+  vector<vector<vector<string> > > data(events, vector<vector<string> >(maxjets, vector<string>(nvars)));
 
-  vector<string> line_data(4);
+  vector<string> line_data(nvars);
   int idx;
 
   for (int i = 0; i < events; i++){
     for (int j = 0; j < maxjets; j++){
       line_data = get_line_data(g1[i*maxjets + j]);
-      for (int k = 0; k < 4; k++){
+      for (int k = 0; k < nvars; k++){
           data[i][j][k] = line_data[k];
           //data[i][j][k] = g1[k+j*4+i*maxjets*4];
         }
